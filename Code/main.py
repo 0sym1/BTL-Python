bac16670b2e853649a8f0e159eea1115fa43a14b
thivu <thipro105@gmail.com>
2025-03-21 11:57:39 +0700

    fix final

--- Code/main.py
+++ Code/main.py
@@ -1,5 +1,11 @@
 from PyQt6.QtWidgets import (QApplication)
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 from UI.EncryptionApp import EncryptionApp
+
 
 if __name__ == "__main__":
     app = QApplication([])
